8bf21f6039bfc40f8bed92f84eff06a9b477725d
Fuzail <fuzailmomin5@gmail.com>
2025-01-23 10:02:03 +0000

    latest

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, My name is Fuzail!"})
+    return jsonify({"message": "Hello, My name is Fuzail Hasan Momin!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
